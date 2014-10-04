f5f46b0b44fa22126e646160608c62ac1392c552
ritvikupadhyaya <ritvikupadhyaya@Ritviks-MacBook-Air.local>
2014-10-04 16:06:22 -0500

    Framework fix

--- openspatial-iOS-SDK-master/Example App Source/MainViewController.h
+++ openspatial-iOS-SDK-master/Example App Source/MainViewController.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "OpenSpatial/OpenSpatialBluetooth.h"
+#import "OpenSpatialBluetooth.h"
 
 @interface MainViewController : UIViewController <OpenSpatialBluetoothDelegate>
 
