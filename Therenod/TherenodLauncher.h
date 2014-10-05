3c93bd73a31265b565cb836de550bc22809617b1
LOZORD <ljrudberg@gmail.com>
2014-10-05 01:01:14 -0400

    Started Therenod Launcher view and controller

---
+++ Therenod/TherenodLauncher.h
@@ -0,0 +1,15 @@
+//
+//  TherenodLauncher.h
+//  Therenod
+//
+//  Created by Leo Rudberg on 10/5/14.
+//  Copyright (c) 2014 LeoAndRitvik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ViewController.h"
+
+@interface TherenodLauncher : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *launchThereminBtn;
+
+@end
---
+++ Therenod/TherenodLauncher.m
@@ -0,0 +1,16 @@
+//
+//  TherenodLauncher.m
+//  Therenod
+//
+//  Created by Leo Rudberg on 10/5/14.
+//  Copyright (c) 2014 LeoAndRitvik. All rights reserved.
+//
+
+#import "TherenodLauncher.h"
+
+@implementation TherenodLauncher
+- (IBAction)launchTherenod:(id)sender {
+    NSLog(@"Let's get this party started");
+}
+
+@end
