44b81c4bc722640181b7f62a485e708dc8eadcec
LOZORD <ljrudberg@gmail.com>
2014-10-05 00:13:56 -0400

    Smooth pitch modulation babayyyyyyy

--- Therenod/ViewController.m
+++ Therenod/ViewController.m
@@ -43,11 +43,21 @@
 }
 
 - (void) playSoundWithPitch:(float)p withVolume:(float)v {
-    [self stopSound]; //otherwise it break :'(
+    //[self stopSound]; //otherwise it break :'(
 
     if (should_play)
     {
-        tg = [[TGSineWaveToneGenerator alloc] initWithFrequency:p amplitude:v];
+        //if we have't initialized the tg yet, create it
+        if (tg == nil)
+        {
+            tg = [[TGSineWaveToneGenerator alloc] initWithFrequency:p amplitude:v];
+        }
+        //otherwise, update the values
+        else
+        {
+            tg->frequency = p;
+            tg->amplitude = v;
+        }
 
         [tg play];
     }
@@ -61,13 +71,11 @@
     [tg stop];
 }
 - (IBAction)pitch_slider:(id)sender {
-    NSLog(@"YOLOSWAG--p");
     pitch = pitch_slider.value;
     [self playSoundWithPitch:pitch withVolume:volume];
 }
 
 - (IBAction)volume_slider:(id)sender {
-    NSLog(@"YOLOSWAG--v");
     volume = volume_slider.value;
     [self playSoundWithPitch:pitch withVolume:volume];
 }
